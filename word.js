071d07ec8dc7efd40c8906834c066490ff1226dc
Araib Nasir <NasirX@MacBooks-MacBook-Pro-4.local>
2015-10-16 09:17:37 -0400

    Updated Thursday

--- word.js
+++ word.js
@@ -1,12 +1,39 @@
 
 //Long Word Game//
 
-var longWords = ['rotavator','perfidiousness','subdermatoglyphic','sesquipedalian',
-'Supercalifragilisticexpialidocius', 'floccinaucinihilipilification', 'pseudopseudohypoparathyroidism', 'parastratiosphecomyiastratiosphecomyioides','Aequeosalinocalcalinosetaceoaluminosocupreovitriolic','Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch']
+// var title = ['Orange', 'Apple', 'Mango', 'Airplane', 'Kiwi'];
+//
+// var i = 0;  // the index of the current item to show
+//
+// setInterval(function() {            // setInterval makes it run repeatedly
+//     document
+//         .getElementById('fruit')
+//         .innerHTML = title[i++];    // get the item and increment
+// }, 1000);
 
-//Possible Issue incoming - i.e. Some words not being able to fit the screen?
+var wordBank = ['rotavator', 'perfidiousness', 'subdermatoglyphic', 'sesquipedalian', 'Supercalifragilisticexpialidocius', 'floccinaucinihilipilification', 'pseudopseudohypoparathyroidism', 'parastratiosphecomyiastratiosphecomyioides', 'Aequeosalinocalcalinosetaceoaluminosocupreovitriolic', 'Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch'];
 
+var i = 0;
+
+setInterval(function() {            // setInterval makes it run repeatedly
+    document.getElementById('wordTest')
+        .innerHTML = wordBank[i++];    // get the item and increment
+    if (i == wordBank.length) i = 0;
+      // reset to first element if you've reached the end
+}, 12000);
 function gameLost() {
-  alert("You loose!");
+  alert("You lose!");
 }
-setTimeout('gameLost()', 60000);
+setTimeout('gameLost()', 120000);
+
+// var foo = prompt("Give me input");
+
+
+
+// Possible Issue incoming - i.e. Some words not being able to fit the display?
+
+
+// function gameLost() {
+//   alert("You lose!");
+// }
+// setTimeout('gameLost()', 60000);
